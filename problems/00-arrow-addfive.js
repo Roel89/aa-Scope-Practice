aa5ff0da75001c1abb9ce0f234a62891457df97b
rbxbx <roelm07@yahoo.com>
2024-04-03 23:47:35 -0600

    00 & 01

--- problems/00-arrow-addfive.js
+++ problems/00-arrow-addfive.js
@@ -16,7 +16,8 @@ let result3 = addFive(37); // returns 42
 ***********************************************************************/
 
 // Your code here 
-
+const addFive = number =>
+  number + 5
 
 let result1 = addFive(0); // returns 5
 let result2 = addFive(10); // returns 15
